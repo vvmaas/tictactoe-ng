87fd632ac96ca607388a1dbe2241ed4c5200ada4
Victor Vermaas <vhvermaas@gmail.com>
2023-07-20 16:46:40 -0300

    implemented base app

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button class="null" *ngIf="!value">
+      {{ value }}
+    </button>
+
+    <button class="X" *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+
+    <button class="O" *ngIf="value == 'O'">
+      {{ value }}
+    </button>
+  `,
+  styles: [
+    `
+    button {
+      width: 100%;
+      height: 100%;
+      font-size: clamp(25px, 15vw, 80px);
+      font-weight: bold;
+      color: white;
+      cursor: pointer;
+    }
+
+    .null {
+      background-color: #071e22;
+    }
+
+    .X {
+      background-color: #1d7874;
+    }
+
+    .O {
+      background-color: #ee2e31;
+    }
+    `
+  ]
+})
+export class SquareComponent {
+
+  @Input() value: 'X' | 'O' | '';
+
+  constructor() {
+    this.value = '';
+  }
+}
